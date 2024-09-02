06eaeac58fcd8224e3b415be5f9419024561fcf4
whthfc <santiago2006rp@gmail.com>
2024-09-01 21:12:48 -0500

    HTML | CSS | JS

--- My First HTML/src/js/main.js
+++ My First HTML/src/js/main.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 function showDate() {
     document.getElementById('demo').innerHTML = Date();
-    alert("Soy Kelly Marin");
+    alert("Soy Santiago Ramirez");
 }
-=======
-function showDate(){
-    document.getElementById('demo').innerText = Date();
-}
-alert("Hello World!");
->>>>>>> a8520664e77afa0b3fd04da0c0647d2411e9eef4
