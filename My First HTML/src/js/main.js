c2f68a55aaae169ab0012cb65c7a5b87d3a09f2e
whthfc <santiago2006rp@gmail.com>
2024-09-01 21:09:17 -0500

    Si

--- My First HTML/src/js/main.js
+++ My First HTML/src/js/main.js
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 function showDate() {
     document.getElementById('demo').innerHTML = Date();
     alert("Soy Kelly Marin");
 }
+=======
+function showDate(){
+    document.getElementById('demo').innerText = Date();
+}
+alert("Hello World!");
+>>>>>>> a8520664e77afa0b3fd04da0c0647d2411e9eef4
